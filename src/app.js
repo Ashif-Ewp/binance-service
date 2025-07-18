11f916098c3408884a5a2c4421f1ea4b3fe0d64a
md-786910 <mdashifreza111@gmail.com>
2025-07-18 12:21:16 +0530

    Fix configure to vercel

--- src/app.js
+++ src/app.js
@@ -121,6 +121,8 @@ app.use((err, req, res, next) => {
 // Listen
 
 const port = process.env.PORT || 8081;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+
+module.exports = app;
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
